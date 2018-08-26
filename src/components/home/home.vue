27a4a75e4e46c186bf0c00922939b68fc3e9f2f0
sundandan <1486620982@qq.com>
2018-08-26 17:12:34 +0800

    登录与退出完成

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -3,7 +3,8 @@
         <el-header class="home-header">
             <el-row class="logo-img">
                 <el-col :span="4"><div class="grid-content bg-purple"><img src="./logo.png" alt=""></div></el-col>
-                <el-col :offset="16" :span="4"><div class="grid-content bg-purple"><a href="">退出</a></div></el-col>
+                <el-col :offset="16" :span="4"><div class="grid-content bg-purple">
+                  <a href="" @click.prevent="logout">退出</a></div></el-col>
               </el-row>
         </el-header>
         <el-container>
@@ -16,6 +17,31 @@
 export default {
   data () {
     return {}
+  },
+  methods: {
+    logout () {
+      this.$confirm('你确认要退出吗?', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        //  删除token
+        window.localStorage.removeItem('admin-token')
+        // 跳转到登录视图
+        this.$router.push({
+          name: 'login'
+        })
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    }
   }
 }
 </script>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -44,6 +44,10 @@ export default{
         this.$router.push({
           name: 'home'
         })
+        this.$message({
+          type: 'success',
+          message: '登录成功!'
+        })
       }
     }
   }
