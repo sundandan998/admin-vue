4aaec5c272d980fc64676dfe6c74298e997f5804
sundandan <1486620982@qq.com>
2018-08-27 10:11:59 +0800

    完成退出以及home页面切换

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,15 +1,76 @@
 <template>
     <el-container class="home-box">
-        <el-header class="home-header">
-            <el-row class="logo-img">
-                <el-col :span="4"><div class="grid-content bg-purple"><img src="./logo.png" alt=""></div></el-col>
-                <el-col :offset="16" :span="4"><div class="grid-content bg-purple">
-                  <a href="" @click.prevent="logout">退出</a></div></el-col>
-              </el-row>
-        </el-header>
-        <el-container>
-          <el-aside class="home-aside">Aside</el-aside>
-          <el-main class="home-main">Main</el-main>
+      <el-header class="home-header">
+          <el-row class="logo-img">
+              <el-col :span="4"><div class="grid-content bg-purple"><img src="./logo.png" alt=""></div></el-col>
+              <el-col :offset="16" :span="4"><div class="grid-content bg-purple">
+                <a href="" @click.prevent="logout">退出</a></div></el-col>
+            </el-row>
+      </el-header>
+      <el-container>
+        <el-aside>
+          <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo aside-menu"
+              @open="handleOpen"
+              @close="handleClose"
+              background-color="#545c64"
+              text-color="#fff"
+              :unique-opened="true"
+              active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>用户管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1">用户列表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>权限管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="2-1">角色列表</el-menu-item>
+                <el-menu-item index="2-2">权限列表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="3">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>商品管理</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="3-1">商品列表</el-menu-item>
+                  <el-menu-item index="3-1">商品分类</el-menu-item>
+                  <el-menu-item index="3-1">商品参数</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+            <el-submenu index="4">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>订单管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="4-1">订单列表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="5">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>数据统计</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="5-1">数据列表</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+            </el-menu>
+            </el-aside>
+          <el-main class="home-main">
+            <router-view></router-view>
+          </el-main>
         </el-container>
     </el-container>
 </template>
@@ -41,6 +102,12 @@ export default {
           message: '已取消删除'
         })
       })
+    },
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
@@ -64,10 +131,13 @@ export default {
   line-height: 60px;
 }
 .home-aside{
+  height: 100%;
   background-color: #D3DCE6;
 }
 .home-main{
   background-color: #E9EEF3;
 }
-
+.aside-menu{
+  height: 100%;
+}
 </style>
